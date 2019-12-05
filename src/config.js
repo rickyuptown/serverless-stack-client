ada51a4e2a48a24405fe058dcc281b2baa7ab49b
ricky_kwok <ricky.gillespie@gmail.com>
2019-12-05 16:28:07 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-uploads-ricky"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://6y7ztf5fyk.execute-api.ap-southeast-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_aVD61AfKz",
+    APP_CLIENT_ID: "6okq1nl80qpfjus70s7o16ivkq",
+    IDENTITY_POOL_ID: "ap-southeast-2:ff1c440f-712a-4673-b5c3-2db5cefe7e79"
+  }
+};
